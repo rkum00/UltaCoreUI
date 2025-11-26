f9fa7bd2a788c9a085aa36261c8c64f42e3f40bd
rkum00 <krakesh@nisum.com>
2025-11-26 19:12:39 +0530

    Added UltaCoreUIExample app

---
+++ Example/UltaCoreUIExample/UltaCoreUIExample/UltaCoreUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltaCoreUIExampleApp.swift
+//  UltaCoreUIExample
+//
+//  Created by Rakesh Kumar on 26/11/25.
+//
+
+import SwiftUI
+
+@main
+struct UltaCoreUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
