b316aef8bee26e89f48ed925caf0419a0737fc83
rkum00 <krakesh@nisum.com>
2025-12-01 19:28:56 +0530

    Added Navigation with ListView having two section Atoms and Molecules

--- Example/UltaCoreUIExample/UltaCoreUIExample/ContentView.swift
+++ Example/UltaCoreUIExample/UltaCoreUIExample/ContentView.swift
@@ -9,16 +9,73 @@ import SwiftUI
 import UltaCoreUI
 
 struct ContentView: View {
+    @State private var expandedSections: Set<String> = []
+    
     var body: some View {
-        VStack(spacing: 20) {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            SwiftUIView()
+        NavigationView {
+            List {
+                SectionView(
+                    title: "Atoms",
+                    items: ["Colors", "Fonts", "Icons"],
+                    isExpanded: expandedSections.contains("Atoms"),
+                    toggleExpand: { toggleSection("Atoms") }
+                )
+                .listRowSeparator(.hidden)
+                
+                SectionView(
+                    title: "Molecules",
+                    items: ["UBText", "UBButton", "UBCardContainer"],
+                    isExpanded: expandedSections.contains("Molecules"),
+                    toggleExpand: { toggleSection("Molecules") }
+                )
+                .listRowSeparator(.hidden)
+            }
+            .listStyle(.plain)
+            .navigationTitle("Home Page")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+    
+    private func toggleSection(_ section: String) {
+        if expandedSections.contains(section) {
+            expandedSections.remove(section)
+        } else {
+            expandedSections.insert(section)
         }
     }
 }
 
-#Preview {
-    ContentView()
+struct SectionView: View {
+    let title: String
+    let items: [String]
+    let isExpanded: Bool
+    let toggleExpand: () -> Void
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text(title)
+                    .font(.headline)
+                Spacer()
+                Image(systemName: isExpanded ? "chevron.up" : "chevron.down")
+                    .onTapGesture {
+                        toggleExpand()
+                    }
+            }
+            .padding(.vertical, 5)
+            .frame(height: 50)
+            
+            if isExpanded {
+                Divider()
+                ForEach(items.indices, id: \.self) { index in
+                    VStack(alignment: .leading) {
+                        Text(items[index])
+                            .padding(.leading, 20)
+                            .frame(height: 40)
+                        Divider()
+                    }
+                }
+            }
+        }
+    }
 }
