5d569ef5fb570b73ea616b6e2c8f64a3c395af3a
rkum00 <krakesh@nisum.com>
2025-12-16 19:30:58 +0530

    added duration to dismiss the banner

--- Sources/UltaCoreUI/Molecules/UBBanner/UBBannerView.swift
+++ Sources/UltaCoreUI/Molecules/UBBanner/UBBannerView.swift
@@ -13,14 +13,18 @@ public struct UBBannerView: View {
     let message: String?
     let type: UBBannerType
     let textColor: TextColorType
+    let duration: TimeInterval?
     let theme: UBTheme
     let onClose: ((UBBannerType) -> Void)?
+    
+    @State private var isVisible: Bool = true
     
     public init(
         title: String,
         message: String?,
         type: UBBannerType,
         textColor: TextColorType,
+        duration: TimeInterval? = nil,
         theme: UBTheme = .current,
         onClose: ((UBBannerType) -> Void)?
     ) {
@@ -28,66 +32,83 @@ public struct UBBannerView: View {
         self.message = message
         self.type = type
         self.textColor = textColor
+        self.duration = duration
         self.theme = theme
         self.onClose = onClose
     }
     
     public var body: some View {
-        HStack(alignment: .top, spacing: UBGlobal.space300) {
-            Image(systemName: type.iconName)
-                .renderingMode(.template)
-                .resizable()
-                .scaledToFit()
-                .frame(
-                    width: UBGlobal.sizeWidth600,
-                    height: UBGlobal.sizeHeight600
-                )
-                .foregroundColor(.white)
-            
-            VStack(alignment: .leading, spacing: UBGlobal.space100) {
-                UBText(textAttribute: TextAttributes(
-                    text: title,
-                    color: textColor,
-                    fontSize: .xlarge,
-                    fontWeight: .bold
-                ))
+        if isVisible {
+            HStack(alignment: .top, spacing: UBGlobal.space300) {
+                Image(systemName: type.iconName)
+                    .renderingMode(.template)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(
+                        width: UBGlobal.sizeWidth600,
+                        height: UBGlobal.sizeHeight600
+                    )
+                    .foregroundColor(.white)
                 
-                if let message {
+                VStack(alignment: .leading, spacing: UBGlobal.space100) {
                     UBText(textAttribute: TextAttributes(
-                        text: message,
+                        text: title,
                         color: textColor,
-                        fontSize: .small,
-                        fontWeight: .regular
+                        fontSize: .xlarge,
+                        fontWeight: .bold
                     ))
-                }
-            }
-            Spacer()
-            if let onClose {
-                Button {
-                    onClose(type)
-                } label: {
-                    ZStack {
-                        Image(systemName: type.crossicon)
-                            .renderingMode(.template)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(
-                                width: UBGlobal.sizeWidth400,
-                                height: UBGlobal.sizeHeight400
-                            )
-                            .scaleEffect(UBGlobal.fontLineHeight25)
-                            .foregroundColor(.white)
+                    
+                    if let message {
+                        UBText(textAttribute: TextAttributes(
+                            text: message,
+                            color: textColor,
+                            fontSize: .small,
+                            fontWeight: .regular
+                        ))
                     }
                 }
-                .buttonStyle(.plain)
+                Spacer()
+                if let onClose {
+                    Button {
+                        dismissBanner()
+                    } label: {
+                        ZStack {
+                            Image(systemName: type.crossicon)
+                                .renderingMode(.template)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(
+                                    width: UBGlobal.sizeWidth400,
+                                    height: UBGlobal.sizeHeight400
+                                )
+                                .scaleEffect(UBGlobal.fontLineHeight25)
+                                .foregroundColor(.white)
+                        }
+                    }
+                    .buttonStyle(.plain)
+                }
+            }
+            .contentShape(Rectangle())
+            .padding()
+            .background(type.backgroundColor)
+            .cornerRadius(UBGlobal.borderRadius300)
+            .shadow(radius: UBGlobal.borderRadius100)
+            .padding(.horizontal)
+            .onAppear {
+                if let duration {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+                        dismissBanner()
+                    }
+                }
             }
         }
-        .contentShape(Rectangle())
-        .padding()
-        .background(type.backgroundColor)
-        .cornerRadius(UBGlobal.borderRadius300)
-        .shadow(radius: UBGlobal.borderRadius100)
-        .padding(.horizontal)
+    }
+    
+    private func dismissBanner() {
+        withAnimation {
+            isVisible = false
+            onClose?(type)
+        }
     }
 }
 
