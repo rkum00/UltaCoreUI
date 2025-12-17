c0e00f8f9040e36eabfcbf538598f238d5a2ca54
rkum00 <krakesh@nisum.com>
2025-12-17 21:32:21 +0530

    modified the UBLoader component

--- Sources/UltaCoreUI/Molecules/UBLoader/UBLoaderView.swift
+++ Sources/UltaCoreUI/Molecules/UBLoader/UBLoaderView.swift
@@ -10,18 +10,18 @@ import SwiftUI
 @available(iOS 15.0, *)
 public struct UBLoaderView: View {
     // MARK: - Private Properties
-    /// This property is used to control the loading state of the spinner component.
+    /// This property is used to control the loading state of the loader component.
     @State private var isLoading = false
     
-    // MARK: - Public Properties
-    /// This property is used to set the theme of the spinner component.
-    public var theme: UBTheme
-    /// This property is used to set the color of the spinner component.
-    public var color: UBLoaderColor = .primary
-    /// This property is used to set the size of the spinner component.
-    public var size: UBLoaderSize = .large
-    /// This property is used to set the label of the spinner component.
-    public var spinnerLabel: UBText?
+    // MARK: - Private Properties
+    /// This property is used to set the theme of the loader component.
+    let theme: UBTheme
+    /// This property is used to set the color of the loader component.
+    let color: UBLoaderColor
+    /// This property is used to set the size of the loader component.
+    let size: UBLoaderSize
+    /// This property is used to set the label of the loader component.
+    let loaderLabel: UBText?
 
     // MARK: - Private Computed Properties
     private var animationDuration: TimeInterval {
@@ -29,16 +29,16 @@ public struct UBLoaderView: View {
     }
     
     private var labelSpacing: CGFloat {
-        spinnerLabel != nil ? UBGlobal.space0 : size.getLabelSpacing()
+        loaderLabel != nil ? UBGlobal.space0 : size.getLabelSpacing()
     }
     
     // MARK: - Initializer
-    ///  This init method is used for customizing the spinner component. Below are the parameter descriptions:
+    ///  This init method is used for customizing the loader component. Below are the parameter descriptions:
     /// - Parameters:
-    ///   - theme: The theme of the spinner. It is used to set color based on the selected theme. Default is `UMATheme.current`.
-    ///   - color: The color of the spinner. It is used to set the indicator and track color. Default is `.primary`.
-    ///   - size: The size of the spinner. It is used to set the radius of the spinner. Default is `.large`.
-    ///   - labelText: Optional text to display below the spinner.
+    ///   - theme: The theme of the loade. It is used to set color based on the selected theme. Default is `UMATheme.current`.
+    ///   - color: The color of the loader. It is used to set the indicator and track color. Default is `.primary`.
+    ///   - size: The size of the loader. It is used to set the radius of the loader. Default is `.large`.
+    ///   - labelText: Optional text to display below the loader.
     public init(
         theme: UBTheme = UBTheme.current,
         color: UBLoaderColor = .primary,
@@ -49,7 +49,7 @@ public struct UBLoaderView: View {
         self.color = color
         self.size = size
         if let labelText, !labelText.isEmpty {
-            spinnerLabel = UBText(textAttribute: TextAttributes(
+            loaderLabel = UBText(textAttribute: TextAttributes(
                 text: labelText,
                 color: color.getTextColor(),
                 fontSize: size.getTextSize(),
@@ -58,20 +58,20 @@ public struct UBLoaderView: View {
                 fontWeight: .semiBold
             ))
         } else {
-            spinnerLabel = nil
+            loaderLabel = nil
         }
     }
     
     // MARK: - Body
     public var body: some View {
         VStack(spacing: labelSpacing) {
-            spinnerCircle
-            spinnerLabel
+            loaderCircle
+            loaderLabel
         }
     }
     
     // MARK: - Private Views
-    private var spinnerCircle: some View {
+    private var loaderCircle: some View {
         ZStack {
             // Background track circle
             Circle()
